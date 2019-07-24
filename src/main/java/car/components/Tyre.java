e6d469c39ce10f353f6a140e5ad62356b9231c0c
dafyddllyr <dafyddllyr@gmail.com>
2019-07-24 12:04:52 +0100

    Tyre class implemented

--- src/main/java/car/components/Tyre.java
+++ src/main/java/car/components/Tyre.java
@@ -1,4 +1,26 @@
 package car.components;
 
 public class Tyre {
+
+    private String model;
+    private String make;
+    private int size;
+
+    public Tyre(String model, String make, int size) {
+        this.model = model;
+        this.make = make;
+        this.size = size;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
--- src/test/java/Car/components/TyreTest.java
+++ src/test/java/Car/components/TyreTest.java
@@ -1,4 +1,32 @@
 package Car.components;
 
+import car.components.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TyreTest {
+
+    Tyre tyre;
+
+    @Before
+    public void setup() {
+        tyre = new Tyre("All Weather Ultra 2000", "Dunlop", 17);
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("All Weather Ultra 2000", tyre.getModel());
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Dunlop", tyre.getMake());
+    }
+
+    @Test
+    public void hasSize() {
+        assertEquals(17, tyre.getSize());
+    }
 }
